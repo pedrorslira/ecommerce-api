d597a18d7e58b986ddb3eb951d6b5af635ddb276
Pedro Ribeiro <pedroribeiro64@outlook.com>
2025-07-25 16:56:53 -0300

    feat: initial commit

---
+++ src/test/java/com/pedroribeiro/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroribeiro.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
