7b2e63fe06e953cf6491ba9ef8004aba24f05911
Pedro Ribeiro <pedroribeiro64@outlook.com>
2025-07-27 13:05:11 -0300

    feat: swagger documentation to get bearer token

---
+++ src/main/java/com/pedroribeiro/ecommerce/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.pedroribeiro.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "Ecommerce API", version = "1.0"),
+    security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"
+)
+public class OpenApiConfig {
+}
--- src/main/java/com/pedroribeiro/ecommerce/controller/AuthController.java
+++ src/main/java/com/pedroribeiro/ecommerce/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.pedroribeiro.ecommerce.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ public class AuthController {
         return authService.authenticate(request);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody AuthRequestDTO body, @RequestParam UserRole role) {
         return authService.register(body, role);
